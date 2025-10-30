35342d1170b75978718fe7fc4ac9aa9e92c1a229
ishuu <ishantgaming371@gmail.com>
2025-10-30 23:32:29 +0530

    Add shop service methods for managing shops, including creation, retrieval by subdomain or ID, listing all shops, and updating shop status.

--- jwls-backend/src/modules/shop/shop.service.ts
+++ jwls-backend/src/modules/shop/shop.service.ts
@@ -10,6 +10,39 @@ import { CreateShopDto } from './dto/create-dhop.dto.js';
 export class ShopService {
     constructor(@InjectModel(Shop.name) private shopModel: Model<shopDocument>) {}
 
-    
-     
+    async findBySubdomain(subDomainorDomain: string) {
+    return this.shopModel.findOne({
+        $or: [
+            {subDomain: subDomainorDomain.toLowerCase()},
+            {customDomain: subDomainorDomain.toLowerCase() },
+        ],
+    })
+    .lean();
+    }
+
+    async createShop(dto: CreateShopDto) {
+        const normalizedSubDomain = dto.subDomain.toLowerCase().replace(/[^a-z0-9-]/g, '');
+        const exists = await this.findBySubdomain(normalizedSubDomain);
+
+        if(exists) throw new ConflictException('Shop with this subDomain or custom domain already exists');
+
+        const create = new this.shopModel({
+            ...dto,
+            subDomain: normalizedSubDomain,
+        });
+
+        return create.save();
+    }
+
+    async listAll() {
+        return this.shopModel.find().lean();
+    } 
+
+    async findById(id: string) {
+        return this.shopModel.findById(id).lean();
+    }
+
+    async updateStatus(id: string, status: string) {
+        return this.shopModel.findByIdAndUpdate(id, {status}, {new: true}).lean();
+    } 
 }
